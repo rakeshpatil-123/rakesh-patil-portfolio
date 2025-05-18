d44c53cf13432be78d173fb710e4540f99b41641
rakeshpatil-123 <rakeshpatil7496@gmail.com>
2025-05-18 09:27:09 +0530

    fix: allow unused variables in remove unused variables to avoid build errors

--- src/components/sections/SocialLinks.tsx
+++ src/components/sections/SocialLinks.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+// import React from 'react';
 import { motion } from 'framer-motion';
 import { SectionWrapper } from '../../hoc';
 import { slideIn } from '../../utils/motion';
